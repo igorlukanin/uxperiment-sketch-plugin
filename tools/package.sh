01f739d953d57498fd1cfc8056fad7059396e027
Igor Lukanin <mail@igor.lukanin.name>
2017-02-16 03:04:12 +0500

    Add tools to view debug logs and package the plugin

---
+++ tools/package.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -R ~/Documents/uxperiment-sketch-plugin/UXperiment.sketchplugin
+cp -R ~/Library/Application\ Support/com.bohemiancoding.sketch3/Plugins/UXperiment.sketchplugin ~/Documents/uxperiment-sketch-plugin/UXperiment.sketchplugin
