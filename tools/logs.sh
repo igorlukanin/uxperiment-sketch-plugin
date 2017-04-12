5171f7781af1bd5773a6340f645c8891f501050e
Igor Lukanin <mail@igor.lukanin.name>
2017-04-12 17:43:14 +0500

    Update Sketch log file location

--- tools/logs.sh
+++ tools/logs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Listening to system log for Sketch Plugin log()'s"
-tail -f /var/log/system.log | grep --line-buffered -E 'Sketch Plugin|^\t' | sed -E "s/.*[[:digit:]]{2} [^ ]+(.*) \(Sketch Plugin\)\[[[:digit:]]+\]/\1/"
+tail -f ~/Library/Logs/com.bohemiancoding.sketch3/Plugin\ Output.log
